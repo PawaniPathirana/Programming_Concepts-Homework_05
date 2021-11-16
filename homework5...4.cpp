2890c75d1099ce5cbbeb7aa3de0b28a023e7ecae
PawaniPathirana <92365939+PawaniPathirana@users.noreply.github.com>
2021-11-16 17:27:00 +0530

    Update homework5...4.cpp

--- homework5...4.cpp
+++ homework5...4.cpp
@@ -1,17 +1,17 @@
 
-/*Design a recursive function that take a positive value of N as 
+/*A program to take a positive value of N as 
 input, and outputs the first N elements of the Fibonacci series.*/
 
 #include<iostream>
 using namespace std;
 
-//phototype function
-int fibonacci(int n);
+
+int fibonacci(int );
 
 int main(){
 	
-	int n,num;             //getting user inputs
-	cout<<"Enter N: ";     //9
+	int n,num;             
+	cout<<"Enter N: ";     
 	cin>>n;
 	cout<<endl;
 	cout<<"The first "<<n<<" elements of the Fibonacci series: " <<fibonacci(n);
@@ -22,7 +22,7 @@ int main(){
 int fibonacci(int n){
 	if(n<=1)
 		return n;
-	int result= fibonacci(n-1)+fibonacci(n-2);//8+ 7+6+ 5+4+ 3+2+ 1
-	return result;     //34
+	int result= fibonacci(n-1)+fibonacci(n-2);
+	return result;     
 }
 
